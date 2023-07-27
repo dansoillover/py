83f3d71024551ec3894b9c89f28d6b80160bc502
Dan <138590525+dansoillover@users.noreply.github.com>
2023-07-27 23:02:17 +0400

    Update prime.py

--- prime.py
+++ prime.py
@@ -2,10 +2,10 @@ x = int(input("Enter a number: "))
 
 if x > 1:
     for i in range(2, int(math.sqrt(num)) + 1):
-        if (num % i) == 0:
-            print(num, "is not a prime number")
+        if (x % i) == 0:
+            print(x, "is not a prime number")
             break
     else:
-        print(num, "is a prime number")
+        print(x, "is a prime number")
 else:
-    print(num, "is not a prime number")
+    print(x, "is not a prime number")
